4f17966d2d057763c92b7d9c752d45ad9edce634
Denis Novikov <voothi@gmail.com>
2019-06-02 13:59:53 +0300

    session fun del

--- public/process.php
+++ public/process.php
@@ -1,6 +1,4 @@
 <?php
-// session_start();
-
 $mysqli = new mysqli("localhost", "root", "", "dbdargs");
 
 if (isset($_POST['save'])) {
@@ -8,17 +6,12 @@ if (isset($_POST['save'])) {
     $location = mysqli_real_escape_string($mysqli, $_POST['location']);
 
     $mysqli->query("INSERT INTO data (name, location) VALUES ('$name', '$location')");
-
-    $_SESSION['message'] = "Record hes been saved";
-    $_SESSION['msg_type'] = "success";
 }
 
 if (isset($_GET['delete'])) {
     $id = mysqli_real_escape_string($mysqli, $_GET['delete']);
 
     $mysqli->query("DELETE FROM data WHERE id=$id");
-    $_SESSION['message'] = "Record hes been deleted";
-    $_SESSION['msg_type'] = "danger";
 }
 
 function pre_r($arr)
